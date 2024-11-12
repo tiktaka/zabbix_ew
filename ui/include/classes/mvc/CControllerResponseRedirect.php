a978fc92498c9bec2bfd2e6435f77526c46222e4
Martins Talbergs <martins.talbergs@zabbix.com>
2024-11-12 12:23:27 +0200

    ..F....... [DEV-4084] fixed redirecting outside Zabbix;removed unused endpoint

--- ui/include/classes/mvc/CControllerResponseRedirect.php
+++ ui/include/classes/mvc/CControllerResponseRedirect.php
@@ -31,6 +31,10 @@ class CControllerResponseRedirect extends CControllerResponse {
 			$location = $location->getUrl();
 		}
 
+		if (!CHtmlUrlValidator::validateSameSite($location)) {
+			throw new CAccessDeniedException();
+		}
+
 		$this->location = $location;
 	}
 
