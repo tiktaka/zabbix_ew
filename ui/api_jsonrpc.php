2a0e1002c4c5e54c91db369d8f5cb5e5c021c6cf
Vladimirs Maksimovs <vladimirs.maksimovs@zabbix.com>
2024-12-13 14:55:45 +0200

    A......... [ZBX-25732] fixed inability to make cross-origin requests to API when passing authentication token using Authorization HTTP header

--- ui/api_jsonrpc.php
+++ ui/api_jsonrpc.php
@@ -20,7 +20,7 @@
 
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Headers: Content-Type,Authorization');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Max-Age: 1000');
 
