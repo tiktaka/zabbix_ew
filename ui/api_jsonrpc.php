2685a0630c6acbe18cbf0ef5e3171ac5d21ba844
Vladimirs Maksimovs <vladimirs.maksimovs@zabbix.com>
2024-12-16 15:30:48 +0200

    A......... [ZBX-25732] fixed inability to make cross-origin HTTP OPTIONS request to API endpoint

--- ui/api_jsonrpc.php
+++ ui/api_jsonrpc.php
@@ -21,7 +21,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type,Authorization');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: POST,OPTIONS');
 header('Access-Control-Max-Age: 1000');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
